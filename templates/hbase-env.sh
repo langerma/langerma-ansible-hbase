b3e959e8cf41721b740a8f32da96accfdc41c4b7
Markus Langer <langer.markus@gmail.com>
2019-03-27 07:02:35 +0100

    added native libs var

--- templates/hbase-env.sh
+++ templates/hbase-env.sh
@@ -41,6 +41,7 @@ export HBASE_HEAPSIZE={{ hbase_heapsize }}
 export HBASE_OPTS="-XX:+UseG1GC"
 
 export HBASE_HOME={{ hbase_install_dir }}/{{ hbase_version }}
+export LD_LIBRARY_PATH=/app/hadoop/lib/native
 
 # Uncomment one of the below three options to enable java garbage collection logging for the server-side processes.
 
